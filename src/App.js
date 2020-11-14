b54dfebcdac8fe1be63b01690b1773fb59faeda8
avtar <avtarakhrisna1@gmail.com>
2020-11-14 18:12:48 +0700

    feat: get data from API using fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CharacterList from './CharacterList';
 import dummyData from './dummyData';
+import endpoint from './endpoint';
 import './App.scss';
 
 function App() {
-  const [characters, setCharacters] = useState(dummyData);
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setCharacters([]);
+    setError(null);
+
+    fetch(endpoint + '/characters')
+      .then((response) => response.json())
+      .then((response) => {
+        setLoading(false);
+        setCharacters(response.characters);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+      });
+  }, []);
 
   return (
     <div className="App">
@@ -13,7 +33,12 @@ function App() {
       </header>
       <main>
         <section className="sidebar">
-          <CharacterList characters={characters} />
+          {loading ? (
+            <p>Loading ..</p>
+          ) : (
+            <CharacterList characters={characters} />
+          )}
+          {error && <p className="error">{error.message}</p>}
         </section>
       </main>
     </div>
